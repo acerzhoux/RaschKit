32ddf80def90aff6f9bc2b012b653b025f571f81
acerzhoux <107518062+acerzhoux@users.noreply.github.com>
2022-06-15 11:29:06 +1000

    Add files via upload

--- R/install_packages_ls.R
+++ R/install_packages_ls.R
@@ -5,6 +5,7 @@
 #' @param packages Vector of package names (character). Default is the vector of packages needed for Package RaschKit.
 #' @examples
 #' install_packages_ls(packages='openxlsx')
+#' @export
 
 install_packages_ls <- function(packages=c('tidyverse', 'conquestr', 'rlang', 'ggthemes', 'ggrepel', 'patchwork', 'gdata', 'janitor', 'data.table', 'RColorBrewer', 'fs')){
     lapply(packages, function(x){
