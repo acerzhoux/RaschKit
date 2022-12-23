697f8b64976008755939af74da5252367760d222
ACER\acerzhoux <xiaoliang.zhou@acer.org>
2022-12-23 15:33:45 +1100

    plot_DIF_group updated

--- R/plot_DIF_group.R
+++ R/plot_DIF_group.R
@@ -12,39 +12,48 @@
 #' @examples
 #' plot_DIF_group(test='elena', DIFVar='quintile')
 #' @export
+
 plot_DIF_group <- function(test, DIFVar, numAbilGrps=3, abilEst2use="pv1"){
     cqs <- conquestr::ConQuestSys(here::here("DIF", DIFVar, paste0(test, '_group.CQS')))
     
     # Person Stats ------------------------------------------------------------
     pStats <- cqs$gAllCaseEstimates %>% {
         tibble(pid = map_int(., "pid"),
-               wle = map_dbl(., "wle"), wleSE = map_dbl(., "wleerr"),
+               wle = map_dbl(., "wle"), 
+               wleSE = map_dbl(., "wleerr"),
                pvs = map(., "pvs"), # plausible values
-               score = map_dbl(., "scores"), maxScore = map_dbl(., "maxscores"))
-    } %>%
+               score = map_dbl(., "scores"), 
+               maxScore = map_dbl(., "maxscores"))
+        } %>%
         ####################################
-    # temporary fix to determine those with score of zero
-    # needs to be revisited often
-    mutate(extr = case_when(wle*2 == -Inf ~ 1,
-                            wle*2 == Inf ~ 1,
-                            TRUE ~ 0)) %>%
+        # temporary fix to determine those with score of zero
+        # needs to be revisited often
+        mutate(extr = case_when(wle*2 == -Inf ~ 1,
+                                wle*2 == Inf ~ 1,
+                                TRUE ~ 0)) %>%
         #####################################
-    # filter(pid %in%  c(1:2)) %>%
-    mutate(pvsCount = map_int(pvs, length),
-           # to ensure that all are captured if there are > 5 pvs
-           pv1 = map_dbl(pvs, 1), pv2 = map_dbl(pvs, 2), pv3 = map_dbl(pvs, 3),
-           pv4 = map_dbl(pvs, 4), pv5 = map_dbl(pvs, 5))
-    
-    # Item Difficulties (iLogit) and deltas -----------------------------------
-    iStepsCounts <- tibble(
-        temp = cqs$gGinLongLabels %>% unlist() %>% str_remove_all(., "item:|(|)"),
-        iStepsCount = cqs$gItemSteps %>% unlist()
-    ) %>%
-        separate(temp, into = c("iNum", "iLab"), sep = " ") %>%
-        mutate(iLab = str_remove_all(iLab, "[(,)]"),
-               iNum = as.numeric(iNum),
-               iType = case_when(iStepsCount == 2 ~ "Dichotomous",
-                                 iStepsCount > 2 ~ "Polytomous"))
+        # filter(pid %in%  c(1:2)) %>%
+        mutate(pvsCount = map_int(pvs, length),
+               # to ensure that all are captured if there are > 5 pvs
+               pv1 = map_dbl(pvs, 1), 
+               pv2 = map_dbl(pvs, 2), 
+               pv3 = map_dbl(pvs, 3),
+               pv4 = map_dbl(pvs, 4), 
+               pv5 = map_dbl(pvs, 5))
+        
+        # Item Difficulties (iLogit) and deltas -----------------------------------
+        iStepsCounts <- tibble(
+            temp = cqs$gGinLongLabels %>% 
+                unlist() %>%
+                str_remove_all(., "item:|(|)"),
+            iStepsCount = cqs$gItemSteps %>% 
+                unlist()
+        ) %>%
+            separate(temp, into = c("iNum", "iLab"), sep = " ") %>%
+            mutate(iLab = str_remove_all(iLab, "[(,)]"),
+                   iNum = as.numeric(iNum),
+                   iType = case_when(iStepsCount == 2 ~ "Dichotomous",
+                                     iStepsCount > 2 ~ "Polytomous"))
     
     # items excluded from calibration
     # OR items in the item pool but not in the test
@@ -84,16 +93,22 @@ plot_DIF_group <- function(test, DIFVar, numAbilGrps=3, abilEst2use="pv1"){
                category = ifelse(is.na(category), 1, category)) %>%
         filter(!is.na(iLab))
     
-    constrained1 <- iStepsCounts %>% filter(iNum == max(iStepsCounts$iNum)) %>%
+    constrained1 <- iStepsCounts %>% 
+        filter(iNum == max(iStepsCounts$iNum)) %>%
         select(iLab) %>%
-        bind_cols(iEstTemp %>% filter(modelTerm == 1) %>%
-                      summarise(Xsi = sum(Xsi)*-1, errorQuick = mean(errorQuick), modelTerm = 1)
+        bind_cols(iEstTemp %>% 
+                      filter(modelTerm == 1) %>%
+                      summarise(Xsi = sum(Xsi)*-1, 
+                                errorQuick = mean(errorQuick), 
+                                modelTerm = 1)
         )
     
     if(2 %in% unique(iEstTemp$modelTerm)){
-        constrained2 <- iEstTemp %>% filter(modelTerm == 2) %>%
+        constrained2 <- iEstTemp %>%
+            filter(modelTerm == 2) %>%
             group_by(iLab) %>%
-            summarise(Xsi = sum(Xsi)*-1, errorQuick = mean(errorQuick),
+            summarise(Xsi = sum(Xsi)*-1, 
+                      errorQuick = mean(errorQuick),
                       category = max(category) + 1) %>%
             ungroup() %>%
             mutate(modelTerm = 2)
@@ -105,8 +120,10 @@ plot_DIF_group <- function(test, DIFVar, numAbilGrps=3, abilEst2use="pv1"){
         bind_rows(constrained1, constrained2)
     
     # if test has only dichomtomously scored items, then CQ's item delta is iLogit (item difficulty)
-    deltas <- iStepsCounts %>% filter(iStepsCount > 0) %>%
-        left_join(iEstimates %>% filter(modelTerm == 1) %>%
+    deltas <- iStepsCounts %>% 
+        filter(iStepsCount > 0) %>%
+        left_join(iEstimates %>% 
+                      filter(modelTerm == 1) %>%
                       rename(iLogit = Xsi) %>%
                       select(-category, -modelTerm),
                   by = "iLab"
@@ -129,14 +146,16 @@ plot_DIF_group <- function(test, DIFVar, numAbilGrps=3, abilEst2use="pv1"){
         rowid_to_column() %>% unnest_longer(col = Value) %>%
         mutate(respPrekey = rowid-1) %>%  # because CQ starts with 0
         select(respPrekey, respCat = Value)
+    
     pidVar_groupTab <- cqs$gGroupData %>% {
         tibble(pid = map_chr(., "CaseNum"), var_group = map_chr(., "GData"))
-    } %>%
-        mutate_all(as.numeric)
+        } %>%
+            mutate_all(as.numeric)
+    
     respDat <- cqs$gResponseData %>% {
         tibble(pid = map_dbl(., "Pid"), resp = map_int(., "Rsp"),
                item = map_int(., "Item"), respPrekey = map_int(., "PreKeyRsp"))
-    } %>%
+        } %>%
         mutate(iNum = item+1) %>% # because CQ starts with item 0
         left_join(preKeyLookUp, by = "respPrekey") %>%
         left_join(deltas %>% distinct(iNum, iLab, iStepsCount),
@@ -174,15 +193,15 @@ plot_DIF_group <- function(test, DIFVar, numAbilGrps=3, abilEst2use="pv1"){
         summarise(Count = n()) %>% ungroup() %>%
         complete(Score, nesting(iNum, group, Group, Ability), fill = list(Count = 0)) %>%
         ########################
-    # removes scores with count of zero across ability groups
-    group_by(iNum, Score) %>% mutate(scoreCount = sum(Count)) %>%
-        filter(scoreCount > 0) %>% select(-scoreCount) %>%
-        ########################
-    group_by(iNum, Ability, Group, group) %>%
-        mutate(prop = Count/sum(Count)) %>%
-        ungroup() %>%
-        arrange(iNum, group, Ability, Score) %>%
-        mutate(Group = str_pad(Group, 2, "left", "0"))
+        # removes scores with count of zero across ability groups
+        group_by(iNum, Score) %>% mutate(scoreCount = sum(Count)) %>%
+            filter(scoreCount > 0) %>% select(-scoreCount) %>%
+            ########################
+        group_by(iNum, Ability, Group, group) %>%
+            mutate(prop = Count/sum(Count)) %>%
+            ungroup() %>%
+            arrange(iNum, group, Ability, Score) %>%
+            mutate(Group = str_pad(Group, 2, "left", "0"))
     
     dfObservedAverage <- ccDat %>% filter(type == abilEst2use) %>%
         group_by(type, group) %>%
@@ -193,30 +212,36 @@ plot_DIF_group <- function(test, DIFVar, numAbilGrps=3, abilEst2use="pv1"){
         summarise(Count = n()) %>% ungroup() %>%
         complete(Score, nesting(iNum, group, Ability), fill = list(Count = 0)) %>%
         ########################
-    # removes scores with count of zero across ability groups
-    group_by(iNum, Score) %>% mutate(scoreCount = sum(Count)) %>%
-        filter(scoreCount > 0) %>% select(-scoreCount) %>%
-        ########################
-    group_by(iNum, Ability, group) %>%
-        mutate(prop = Count/sum(Count)) %>%
-        ungroup() %>%
-        arrange(iNum, group, Ability, Score)
+        # removes scores with count of zero across ability groups
+        group_by(iNum, Score) %>% 
+        mutate(scoreCount = sum(Count)) %>%
+            filter(scoreCount > 0) %>% 
+        select(-scoreCount) %>%
+            ########################
+        group_by(iNum, Ability, group) %>%
+            mutate(prop = Count/sum(Count)) %>%
+            ungroup() %>%
+            arrange(iNum, group, Ability, Score)
     
-    xMin <- round(min( dfObserved$Ability[dfObserved$Ability>-10]), 2) - 0.1
+    xMin <- round(min(dfObserved$Ability), 2) - 0.1
     xMax <- round(max(dfObserved$Ability), 2) + 0.1
     
     pdf(file=here::here('DIF', paste0(DIFVar, '_', test, "_Group.pdf")),
         width = 10, height = 7)
     
     for(k in unique(deltas$iNum)){
-        i_df <- deltas %>% filter(iNum == k)
-        o_df <- dfObserved %>% filter(iNum == k, Score == 1, !is.na(Group)) %>%
-            bind_rows(dfObservedAverage %>% filter(iNum == k, Score == 1) %>%
+        i_df <- deltas %>% 
+            filter(iNum == k)
+        o_df <- dfObserved %>% 
+            filter(iNum == k, Score == 1, !is.na(Group)) %>%
+            bind_rows(dfObservedAverage %>% 
+                          filter(iNum == k, Score == 1) %>%
                           mutate(Group = "Average\nExpected")) %>%
             mutate(lineLab = "Observed\nProportion")
         
-        m_df <- tibble(iNum = k, ability = seq(xMin, xMax, by=.02)) %>%
-            mutate(pSuccess = exp(ability - unique(i_df$iLogit))/(1 + exp(ability - unique(i_df$iLogit))),
+        m_df <- tibble(iNum = k, 
+                       ability = seq(xMin, xMax, by=.02)) %>%
+            mutate(pSuccess = exp(ability - i_df$iLogit[[1]])/(1+exp(ability - i_df$iLogit[[1]])),
                    lineLab = "Model\nProbability")
         
         colourCount = length(unique(o_df$Group))
